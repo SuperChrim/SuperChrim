e6d7964d116a0eb51bcb0e149e766a027b6c2f1f
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2023-08-22 01:00:30 -0400

    Th19 story BGM change

---
+++ Th19StoryBGM/patch.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "Th19 story BGM change",
+  "servers": [
+    "https://mirrors.thpatch.net/SuperChrim/Th19StoryBGMChange/"
+  ],
+  "title": "Th19 story BGM Change \n Changes the first couple stage BGMs to each story to their respective opponent themes."
+}
--- repo.js
+++ repo.js
@@ -7,6 +7,7 @@
     ],
     "patches": {
         "House Set OST": "House Set remix music (Composed by Kuroneko Lounge). \n Compatitible games: th6, th7, th8, th9, th10, th11, th12, th13, th14, th143, th15, th16",
-        "Th19 story AI swap": "A silly little mod that makes you control player 2 in Touhou 19 story mode."
+        "Th19 story AI swap": "A silly little mod that makes you control player 2 in Touhou 19 story mode.",
+        "Th19 story BGM change": "Changes the first couple stage BGMs to each story to their respective opponent themes."
     }
 }
