5c5dd369d50d45773ec0a696e8e982e14cd6b2d4
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2023-08-27 14:35:33 -0400

    New th19 big portraits patch

---
+++ Th19BigPortraits/ReadMe.txt
@@ -0,0 +1,5 @@
+Set PortraitSetting in either config or th19.v1.00a.js to customize how the big portaits will appear.
+0 = Turn the patch off.
+1 = Big portraits for all attacks.
+2 = Big portraits for boss attacks only.
+3 = Big portraits for boss attacks only in story mode.
---
+++ Th19BigPortraits/patch.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "Th19 Big Portraits",
+  "servers": [
+    "https://mirrors.thpatch.net/SuperChrim/Th19BigPortraits/"
+  ],
+  "title": "Th19 Big Portraits \n Adds a new big version of the character portraits for boss attacks. (Customization options included)"
+}
