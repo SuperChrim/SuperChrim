6e6c3fd6c92b8f98add0e9b1d27147648fcb58d2
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2023-09-11 04:35:20 -0400

    Update patch.js

--- HouseSetOST/patch.js
+++ HouseSetOST/patch.js
@@ -2,9 +2,9 @@
   "dependencies": [
     "nmlgc/base_tsa"
   ],
-  "id": "HouseSet",
+  "id": "HouseSetOST",
   "servers": [
-    "https://mirrors.thpatch.net/SuperChrim/HouseSet/"
+    "https://mirrors.thpatch.net/SuperChrim/HouseSetOST/"
   ],
   "title": "House Set remixes (Composed by Kuroneko Lounge) \n Compatitible games: th6, th7, th8, th9, th10, th11, th12, th13, th14, th143, th15, th16"
 }
