6a6d6697085f767474c496ebdd16f9ffbbd4f1c6
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2023-09-11 08:36:46 -0400

    Update repo.js

--- repo.js
+++ repo.js
@@ -9,7 +9,7 @@
         "HouseSetOST": "House Set remix music (Composed by Kuroneko Lounge). \n Compatitible games: th6, th7, th8, th9, th10, th11, th12, th13, th14, th143, th15, th16",
         "Th19BigPortraits": "Adds a new big version of the character portraits for boss attacks. (Customization options included)",
         "Th19Chaos": "Enemy waves spawn in all the time with no wait in between. Absolute chaos!",
-        "Th19StoryAIswap": "A silly little mod that makes you control player 2 in Touhou 19 story mode.",
+        "Th19StoryAISwap": "A silly little mod that makes you control player 2 in Touhou 19 story mode.",
         "Th19StoryBalance": "A few tweaks to story mode balance.",
         "Th19StoryBGMChange": "Changes the first couple stage BGMs of each story to their respective opponent themes."
     }
