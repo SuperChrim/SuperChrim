bae89aed890478350f2e7d8803b479687c41e17e
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2023-09-11 04:33:18 -0400

    HouseSetOST rename

--- repo.js
+++ repo.js
@@ -6,7 +6,7 @@
         "https://mirrors.thpatch.net/SuperChrim/"
     ],
     "patches": {
-        "House Set OST": "House Set remix music (Composed by Kuroneko Lounge). \n Compatitible games: th6, th7, th8, th9, th10, th11, th12, th13, th14, th143, th15, th16",
+        "HouseSetOST": "House Set remix music (Composed by Kuroneko Lounge). \n Compatitible games: th6, th7, th8, th9, th10, th11, th12, th13, th14, th143, th15, th16",
         "Th19BigPortraits": "Adds a new big version of the character portraits for boss attacks. (Customization options included)",
         "Th19Chaos": "Enemy waves spawn in all the time with no wait in between. Absolute chaos!",
         "Th19StoryAIswap": "A silly little mod that makes you control player 2 in Touhou 19 story mode.",
