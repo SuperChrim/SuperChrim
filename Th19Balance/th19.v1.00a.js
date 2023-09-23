170743107acb03dd513481a1ac16b56015a3be21
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2023-09-22 23:06:01 -0400

    Create th19.v1.00a.js

---
+++ Th19Balance/th19.v1.00a.js
@@ -0,0 +1,17 @@
+{
+	"binhacks": {
+		"change_jmp_Orinlv2": {
+		"addr": "Rx1A45A",
+		"code": "7E 77"
+		},
+		"else_condition_OrinLv2_jmp": {
+		"addr": "Rx1A4D3",
+		"code": "E9 [codecave:else_condition_OrinLv2]"
+		}
+  },
+  "codecaves": {
+	  "else_condition_OrinLv2": {
+		"code": "C7 86 D8000000 00000000 31 C0 5E C3"
+		}
+  }
+}
