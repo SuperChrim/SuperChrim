a7127e61372c48141adcf6d42ba10898aa0c9ccd
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2023-09-26 09:43:53 -0400

    didn't work

--- Th19Balance/patch.js
+++ Th19Balance/patch.js
@@ -1,7 +1,7 @@
 {
   "id": "Th19Balance",
   "servers": [
-    "https://mirrors.thpatch.net/SuperChrim/Th19Balance/?cjzeioygheiozgjezoigjvze=cizojeogjzeigzg"
+    "https://mirrors.thpatch.net/SuperChrim/Th19Balance/"
   ],
   "title": "Tweaks to characters to make VS mode more balanced"
 }
--- Th19StoryBalance/patch.js
+++ Th19StoryBalance/patch.js
@@ -1,7 +1,7 @@
 {
   "id": "Th19StoryBalance",
   "servers": [
-    "https://mirrors.thpatch.net/SuperChrim/Th19StoryBalance/?cjzeioygheiozgjezoigjvze=cizojeogjzeigzg"
+    "https://mirrors.thpatch.net/SuperChrim/Th19StoryBalance/"
   ],
   "title": "A few tweaks to story mode balance."
 }
