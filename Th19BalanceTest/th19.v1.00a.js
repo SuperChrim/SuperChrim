6b7ada0e140b6f21ac5e8b698c52f82597401964
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2024-05-07 08:38:11 -0400

    file name change

--- Th19BalanceTest/th19.v1.00a.js
+++ Th19BalanceTest/th19.v1.00a.js
@@ -606,4 +606,164 @@
       "code": "<option:BuffHisamiCard2Mul>"
     }
   }
+},
+
+///////////////// Alternate ECL Files /////////////////
+{
+    "options": {
+    "ReimuECLFilename_str": {
+      "type": "s",
+      "val": "pl00_balance.ecl"
+    },
+    "MarisaECLFilename_str": {
+      "type": "s",
+      "val": "pl01_balance.ecl"
+    },
+    "SanaeECLFilename_str": {
+      "type": "s",
+      "val": "pl02_balance.ecl"
+    },
+    "RanECLFilename_str": {
+      "type": "s",
+      "val": "pl03_balance.ecl"
+    },
+    "AunnECLFilename_str": {
+      "type": "s",
+      "val": "pl04_balance.ecl"
+    },
+    "NazrinECLFilename_str": {
+      "type": "s",
+      "val": "pl05_balance.ecl"
+    },
+    "SeiranECLFilename_str": {
+      "type": "s",
+      "val": "pl06_balance.ecl"
+    },
+    "OrinECLFilename_str": {
+      "type": "s",
+      "val": "pl07_balance.ecl"
+    },
+    "TsukasaECLFilename_str": {
+      "type": "s",
+      "val": "pl08_balance.ecl"
+    },
+    "MamizouECLFilename_str": {
+      "type": "s",
+      "val": "pl09_balance.ecl"
+    },
+    "YachieECLFilename_str": {
+      "type": "s",
+      "val": "pl10_balance.ecl"
+    },
+    "SakiECLFilename_str": {
+      "type": "s",
+      "val": "pl11_balance.ecl"
+    },
+    "YuumaECLFilename_str": {
+      "type": "s",
+      "val": "pl12_balance.ecl"
+    },
+    "SuikaECLFilename_str": {
+      "type": "s",
+      "val": "pl13_balance.ecl"
+    },
+    "BitenECLFilename_str": {
+      "type": "s",
+      "val": "pl14_balance.ecl"
+    },
+    "EnokoECLFilename_str": {
+      "type": "s",
+      "val": "pl15_balance.ecl"
+    },
+    "ChiyariECLFilename_str": {
+      "type": "s",
+      "val": "pl16_balance.ecl"
+    },
+    "HisamiECLFilename_str": {
+      "type": "s",
+      "val": "pl17_balance.ecl"
+    },
+    "ZanmuECLFilename_str": {
+      "type": "s",
+      "val": "pl18_balance.ecl"
+    }
+  },
+  "binhacks": {
+    "ReimuECLFilename": {
+      "addr": "Rx1A2B94",
+      "code": "<option:ReimuECLFilename_str>"
+    },
+    "MarisaECLFilename": {
+      "addr": "Rx1A2BB4",
+      "code": "<option:MarisaECLFilename_str>"
+    },
+    "SanaeECLFilename": {
+      "addr": "Rx1A2BD4",
+      "code": "<option:SanaeECLFilename_str>"
+    },
+    "RanECLFilename": {
+      "addr": "Rx1A2BF4",
+      "code": "<option:RanECLFilename_str>"
+    },
+    "AunnECLFilename": {
+      "addr": "Rx1A2C14",
+      "code": "<option:AunnECLFilename_str>"
+    },
+    "NazrinECLFilename": {
+      "addr": "Rx1A2C34",
+      "code": "<option:NazrinECLFilename_str>"
+    },
+    "SeiranECLFilename": {
+      "addr": "Rx1A2C54",
+      "code": "<option:SeiranECLFilename_str>"
+    },
+    "OrinECLFilename": {
+      "addr": "Rx1A2C74",
+      "code": "<option:OrinECLFilename_str>"
+    },
+    "TsukasaECLFilename": {
+      "addr": "Rx1A2C94",
+      "code": "<option:TsukasaECLFilename_str>"
+    },
+    "MamizouECLFilename": {
+      "addr": "Rx1A2CB4",
+      "code": "<option:MamizouECLFilename_str>"
+    },
+    "YachieECLFilename": {
+      "addr": "Rx1A2CD4",
+      "code": "<option:YachieECLFilename_str>"
+    },
+    "SakiECLFilename": {
+      "addr": "Rx1A2CF4",
+      "code": "<option:SakiECLFilename_str>"
+    },
+    "YuumaECLFilename": {
+      "addr": "Rx1A2D14",
+      "code": "<option:YuumaECLFilename_str>"
+    },
+    "SuikaECLFilename": {
+      "addr": "Rx1A2D34",
+      "code": "<option:SuikaECLFilename_str>"
+    },
+    "BitenECLFilename": {
+      "addr": "Rx1A2D54",
+      "code": "<option:BitenECLFilename_str>"
+    },
+    "EnokoECLFilename": {
+      "addr": "Rx1A2D74",
+      "code": "<option:EnokoECLFilename_str>"
+    },
+    "ChiyariECLFilename": {
+      "addr": "Rx1A2D94",
+      "code": "<option:ChiyariECLFilename_str>"
+    },
+    "HisamiECLFilename": {
+      "addr": "Rx1A2DB4",
+      "code": "<option:HisamiECLFilename_str>"
+    },
+    "ZanmuECLFilename": {
+      "addr": "Rx1A2DD4",
+      "code": "<option:ZanmuECLFilename_str>"
+    }
+  }
 }
