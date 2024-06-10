9b21347394861070b309a40d3fcd2e234e32d078
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2024-06-10 15:17:54 -0400

    story binhack organization

---
+++ Th19StoryBalance/th19.js
@@ -0,0 +1,80 @@
+{
+  "options": {
+    "ReimuECLFilename_str": {
+      "type": "s",
+      "val": "pl00_balance_story.ecl"
+    },
+    "MarisaECLFilename_str": {
+      "type": "s",
+      "val": "pl01_balance_story.ecl"
+    },
+    "SanaeECLFilename_str": {
+      "type": "s",
+      "val": "pl02_balance_story.ecl"
+    },
+    "RanECLFilename_str": {
+      "type": "s",
+      "val": "pl03_balance_story.ecl"
+    },
+    "AunnECLFilename_str": {
+      "type": "s",
+      "val": "pl04_balance_story.ecl"
+    },
+    "NazrinECLFilename_str": {
+      "type": "s",
+      "val": "pl05_balance_story.ecl"
+    },
+    "SeiranECLFilename_str": {
+      "type": "s",
+      "val": "pl06_balance_story.ecl"
+    },
+    "OrinECLFilename_str": {
+      "type": "s",
+      "val": "pl07_balance_story.ecl"
+    },
+    "TsukasaECLFilename_str": {
+      "type": "s",
+      "val": "pl08_balance_story.ecl"
+    },
+    "MamizouECLFilename_str": {
+      "type": "s",
+      "val": "pl09_balance_story.ecl"
+    },
+    "YachieECLFilename_str": {
+      "type": "s",
+      "val": "pl10_balance_story.ecl"
+    },
+    "SakiECLFilename_str": {
+      "type": "s",
+      "val": "pl11_balance_story.ecl"
+    },
+    "YuumaECLFilename_str": {
+      "type": "s",
+      "val": "pl12_balance_story.ecl"
+    },
+    "SuikaECLFilename_str": {
+      "type": "s",
+      "val": "pl13_balance_story.ecl"
+    },
+    "BitenECLFilename_str": {
+      "type": "s",
+      "val": "pl14_balance_story.ecl"
+    },
+    "EnokoECLFilename_str": {
+      "type": "s",
+      "val": "pl15_balance_story.ecl"
+    },
+    "ChiyariECLFilename_str": {
+      "type": "s",
+      "val": "pl16_balance_story.ecl"
+    },
+    "HisamiECLFilename_str": {
+      "type": "s",
+      "val": "pl17_balance_story.ecl"
+    },
+    "ZanmuECLFilename_str": {
+      "type": "s",
+      "val": "pl18_balance_story.ecl"
+    }
+  }
+}
