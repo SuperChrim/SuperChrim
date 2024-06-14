c156352daa4e4a7d5acc1354d060f611859ba8c6
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2024-06-14 13:10:19 -0400

    Aunn new card

--- Th19BalanceTest/th19/spells.js
+++ Th19BalanceTest/th19/spells.js
@@ -1,4 +1,5 @@
 {
     "1+BossCard2+0": "Star Sign \"Shoot The Beast Emperor\"",
-    "4+BossCard2+0": "Spinning Top \"Curl Up And Die\""
+    "4+BossCard2+0": "Spinning Top \"Curl Up And Die\"",
+    "4+BossCard3+0": "Koma Sign \"Solitary A-Un Breathing\""
 }
