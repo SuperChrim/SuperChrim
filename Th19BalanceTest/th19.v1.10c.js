f510830c7d8a80288ae58f10ccd40fc5ec8ba995
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2024-08-12 08:53:01 -0400

    balance test v1.10c update

---
+++ Th19BalanceTest/th19.v1.10c.js
@@ -0,0 +1,160 @@
+{
+  "options": {
+    "ReimuECLFilename_str": {
+      "type": "s",
+      "val": "pl00_balanceC.ecl"
+    },
+    "MarisaECLFilename_str": {
+      "type": "s",
+      "val": "pl01_balanceC.ecl"
+    },
+    "SanaeECLFilename_str": {
+      "type": "s",
+      "val": "pl02_balanceC.ecl"
+    },
+    "RanECLFilename_str": {
+      "type": "s",
+      "val": "pl03_balanceC.ecl"
+    },
+    "AunnECLFilename_str": {
+      "type": "s",
+      "val": "pl04_balanceC.ecl"
+    },
+    "NazrinECLFilename_str": {
+      "type": "s",
+      "val": "pl05_balanceC.ecl"
+    },
+    "SeiranECLFilename_str": {
+      "type": "s",
+      "val": "pl06_balanceC.ecl"
+    },
+    "OrinECLFilename_str": {
+      "type": "s",
+      "val": "pl07_balanceC.ecl"
+    },
+    "TsukasaECLFilename_str": {
+      "type": "s",
+      "val": "pl08_balanceC.ecl"
+    },
+    "MamizouECLFilename_str": {
+      "type": "s",
+      "val": "pl09_balanceC.ecl"
+    },
+    "YachieECLFilename_str": {
+      "type": "s",
+      "val": "pl10_balanceC.ecl"
+    },
+    "SakiECLFilename_str": {
+      "type": "s",
+      "val": "pl11_balanceC.ecl"
+    },
+    "YuumaECLFilename_str": {
+      "type": "s",
+      "val": "pl12_balanceC.ecl"
+    },
+    "SuikaECLFilename_str": {
+      "type": "s",
+      "val": "pl13_balanceC.ecl"
+    },
+    "BitenECLFilename_str": {
+      "type": "s",
+      "val": "pl14_balanceC.ecl"
+    },
+    "EnokoECLFilename_str": {
+      "type": "s",
+      "val": "pl15_balanceC.ecl"
+    },
+    "ChiyariECLFilename_str": {
+      "type": "s",
+      "val": "pl16_balanceC.ecl"
+    },
+    "HisamiECLFilename_str": {
+      "type": "s",
+      "val": "pl17_balanceC.ecl"
+    },
+    "ZanmuECLFilename_str": {
+      "type": "s",
+      "val": "pl18_balanceC.ecl"
+    }
+  },
+
+  "binhacks": {
+/////////////////////////////////////////////////////////
+    "ReimuECLFilename": {
+      "addr": "Rx1C5C24",
+      "code": "<option:ReimuECLFilename_str>"
+    },
+    "MarisaECLFilename": {
+      "addr": "Rx1C5C44",
+      "code": "<option:MarisaECLFilename_str>"
+    },
+    "SanaeECLFilename": {
+      "addr": "Rx1C5C64",
+      "code": "<option:SanaeECLFilename_str>"
+    },
+    "RanECLFilename": {
+      "addr": "Rx1C5C84",
+      "code": "<option:RanECLFilename_str>"
+    },
+    "AunnECLFilename": {
+      "addr": "Rx1C5CA4",
+      "code": "<option:AunnECLFilename_str>"
+    },
+    "NazrinECLFilename": {
+      "addr": "Rx1C5CC4",
+      "code": "<option:NazrinECLFilename_str>"
+    },
+    "SeiranECLFilename": {
+      "addr": "Rx1C5CE4",
+      "code": "<option:SeiranECLFilename_str>"
+    },
+    "OrinECLFilename": {
+      "addr": "Rx1C5D04",
+      "code": "<option:OrinECLFilename_str>"
+    },
+    "TsukasaECLFilename": {
+      "addr": "Rx1C5D24",
+      "code": "<option:TsukasaECLFilename_str>"
+    },
+    "MamizouECLFilename": {
+      "addr": "Rx1C5D44",
+      "code": "<option:MamizouECLFilename_str>"
+    },
+    "YachieECLFilename": {
+      "addr": "Rx1C5D64",
+      "code": "<option:YachieECLFilename_str>"
+    },
+    "SakiECLFilename": {
+      "addr": "Rx1C5D84",
+      "code": "<option:SakiECLFilename_str>"
+    },
+    "YuumaECLFilename": {
+      "addr": "Rx1C5DA4",
+      "code": "<option:YuumaECLFilename_str>"
+    },
+    "SuikaECLFilename": {
+      "addr": "Rx1C5DC4",
+      "code": "<option:SuikaECLFilename_str>"
+    },
+    "BitenECLFilename": {
+      "addr": "Rx1C5DE4",
+      "code": "<option:BitenECLFilename_str>"
+    },
+    "EnokoECLFilename": {
+      "addr": "Rx1C5E04",
+      "code": "<option:EnokoECLFilename_str>"
+    },
+    "ChiyariECLFilename": {
+      "addr": "Rx1C5E24",
+      "code": "<option:ChiyariECLFilename_str>"
+    },
+    "HisamiECLFilename": {
+      "addr": "Rx1C5E44",
+      "code": "<option:HisamiECLFilename_str>"
+    },
+    "ZanmuECLFilename": {
+      "addr": "Rx1C5E64",
+      "code": "<option:ZanmuECLFilename_str>"
+    }
+  }
+}
--- Th19BalanceTest/th19/spells.js
+++ Th19BalanceTest/th19/spells.js
@@ -1,6 +1,4 @@
 {
     "1+BossCard2+0": "Star Sign \"Shoot The Beast Emperor\"",
-    "1+BossCard3+0": "Loving Beast \"Ghost Spark\"",
-    "4+BossCard2+0": "Spinning Top \"Curl Up And Die\"",
-    "4+BossCard3+0": "Koma Sign \"Solitary A-Un Breathing\""
+    "4+BossCard2+0": "Spinning Top \"Curl Up And Die\""
 }
