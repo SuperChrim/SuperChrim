405a21e93dc40d9fc19b13e79e0ded73f2358c1b
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2026-07-13 19:07:39 -0400

    Hisami new story spell

--- Th19Balance/th19/spells.js
+++ Th19Balance/th19/spells.js
@@ -1,6 +1,10 @@
 {
     "1+BossCard2+0": "Star Sign \"Shoot The Beast Emperor\"",
     "1+BossCard3+0": "Loving Beast \"Ghost Spark\"",
+    "2+BossCard2+0": "Wind Sign \"Stream Ruler\"",
+    "2+BossCard3+0": "Great Miracle \"Angel's Wind\"",
     "4+BossCard2+0": "Spinning Top \"Curl Up And Die\"",
-    "4+BossCard3+0": "Koma Sign \"Solitary A-Un Breathing\""
+    "4+BossCard3+0": "Koma Sign \"Solitary A-Un Breathing\"",
+    "8+BossCard2+0": "Kuda-gitsune \"Long Cylinder Fox\"",
+    "17+BossCard2+0": "Executor \"Circular Grip\""
 }
--- Th19BalanceTest/th19/spells.js
+++ Th19BalanceTest/th19/spells.js
@@ -5,5 +5,6 @@
     "2+BossCard3+0": "Great Miracle \"Angel's Wind\"",
     "4+BossCard2+0": "Spinning Top \"Curl Up And Die\"",
     "4+BossCard3+0": "Koma Sign \"Solitary A-Un Breathing\"",
-    "8+BossCard2+0": "Kuda-gitsune \"Long Cylinder Fox\""
+    "8+BossCard2+0": "Kuda-gitsune \"Long Cylinder Fox\"",
+    "17+BossCard2+0": "Executor \"Circular Grip\""
 }
