47ac61aa52f19f4f53e9817af8992d42aed6fabc
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2026-07-16 10:34:19 -0400

    Chiyari spell 2 name

--- Th19BalanceTest/th19/spells.js
+++ Th19BalanceTest/th19/spells.js
@@ -6,5 +6,6 @@
     "4+BossCard2+0": "Spinning Top \"Curl Up And Die\"",
     "4+BossCard3+0": "Koma Sign \"Solitary A-Un Breathing\"",
     "8+BossCard2+0": "Kuda-gitsune \"Long Cylinder Fox\"",
+    "16+BossCard2+0": "Cursed Blood \"Cursed Bloody Fire\"",
     "17+BossCard2+0": "Executor \"Circular Grip\""
 }
