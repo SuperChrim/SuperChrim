42b4b32723ebfe98e22fba01802a9f807349d025
SuperChrim <70035611+SuperChrim@users.noreply.github.com>
2026-07-22 08:13:57 -0400

    Seiran spell2 name

--- Th19BalanceTest/th19/spells.js
+++ Th19BalanceTest/th19/spells.js
@@ -5,6 +5,7 @@
     "2+BossCard3+0": "Great Miracle \"Angel's Wind\"",
     "4+BossCard2+0": "Spinning Top \"Curl Up And Die\"",
     "4+BossCard3+0": "Koma Sign \"Solitary A-Un Breathing\"",
+    "6+BossCard2+0": "Gun Sign \"Beast Shooting\"",
     "8+BossCard2+0": "Kuda-gitsune \"Long Cylinder Fox\"",
     "14+BossCard2+0": "Monkey Strike \"Great Monkey Mania\"",
     "15+BossCard2+0": "Three Heads \"Cerberus Inferno\"",
